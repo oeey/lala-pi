76d5352188fb4e210f545ad3315f7c67b4881ae6
Kenneth Burgener <kenneth@oeey.com>
2013-12-24 21:14:34 -0700

    add pandora wrapper

--- pandora.py
+++ pandora.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-TODO TODO
-
 #
 # lala-pi - test.py
 # ZeroMQ Test Client
@@ -9,44 +7,105 @@ TODO TODO
 #
 
 
-import os
+#import os
 import sys
 import time
+#import signal
+import subprocess
 
 import zmq
 
 import common
-from common import log
+log = common.log
 
-common.setup_log('ZMQ_TEST')
+# apt-get install pianobar
+# mkdir -p ~/.config/pianobar
+# mkfifo ~/.config/pianobar/ctl
+# vim ~/.config/pianobar/config
+#   user = [USERNAME]
+#   password = [PASSWORD]
+#   tls_fingerprint = 2D0AFDAFA16F4B5C0A43F3CB1D4752F9535507C0
 
-context = zmq.Context()
+# make Pychecker safe
+if __name__ == "__main__":
 
-z_recv = context.socket(zmq.SUB)
-z_recv.connect("tcp://localhost:5555")
-z_recv.setsockopt(zmq.SUBSCRIBE, '')  # subscribe to everything
+    common.setup_log('ZMQ_PANDORA')
 
-z_send = context.socket(zmq.PUB)
-z_send.connect("tcp://localhost:5556")
+    log("Starting ZMQ Pandora...!")
 
+    common.deamonize()
 
-log("ZMQ Client Started!")
+    context = zmq.Context()
 
-while True:
-    sys.stdout.write("Message: ")
-    message = raw_input().strip()
+    z_recv = context.socket(zmq.SUB)
+    z_recv.connect("tcp://localhost:5555")
+    z_recv.setsockopt(zmq.SUBSCRIBE, 'KEY:')  # subscribe to everything
 
-    if message:
+    z_send = context.socket(zmq.PUB)
+    z_send.connect("tcp://localhost:5556")
+
+    p = subprocess.Popen('/usr/bin/pianobar', shell=True,
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+
+    def clean_shutdown():
+        global z_send
+        global z_recv
+        global context
+        global p
+        log('Quitting...')
+        log('Closing ZQM...')
+        z_send.close()
+        z_recv.close()
+        context.term()
+        log('Closing Pandora...')
+        p.kill()
+        #p.terminate()
+        #os.kill(p.pid, signal.SIGTERM)
+        log('Goodbye.')
+        sys.exit(0)
+    common.SHUTDOWN_CALLBACK = clean_shutdown
+
+    poller = zmq.Poller()
+    poller.register(z_recv, zmq.POLLIN)
+
+    log("ZMQ Pandora Started!")
+    while p.poll() is None:
+
+        message = None
+        #log('.')
+
         try:
-            log('SEND:' + message)
-            z_send.send(message)
+            #in_message = z_recv.recv(zmq.DONTWAIT)
+            # wait 10 seconds for incomming message
+            evts = poller.poll(10000)
+            if evts:
+                message = z_recv.recv()
+                log('RECV: ' + message)
         except zmq.ZMQError as err:
-            log('Send error: ' + str(err))
+            #pass
+            log('Receive error: ' + str(err))
 
-    try:
-        in_message = z_recv.recv(zmq.DONTWAIT)
-        log('RECV:' + in_message)
-    except zmq.ZMQError as err:
-        #pass
-        log('Receive error: ' + str(err))
+        if message:
+            # go to next song
+            if message == "KEY: 6" or message == "KEY: N6":
+                log('Next Song...')
+                with open('/root/.config/pianobar/ctl', 'a') as f:
+                    f.write('n')
+            else:
+                # assume we need to quit
+                with open('/root/.config/pianobar/ctl', 'a') as f:
+                    f.write('q')
+                time.sleep(1)
+                clean_shutdown()
 
+            #try:
+            #    log('SEND:' + message)
+            #    z_send.send(message)
+            #except zmq.ZMQError as err:
+            #    log('Send error: ' + str(err))
+
+    log("Pandora quit unexpectedly...")
+    clean_shutdown()
+
+sys.exit(0)
